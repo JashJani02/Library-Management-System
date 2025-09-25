4894cd1359c68bf9580bad0891411b6c57bc73a1
Jash Jani <jash.janee@gmail.com>
2025-09-25 12:59:57 +0530

    Update user_routes.py

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -2,13 +2,13 @@ from flask import Blueprint, render_template, request, redirect, url_for, curren
 
 user_blueprint = Blueprint("user", __name__, template_folder="../templates/user")
 
-# 👤 User Dashboard
+
 @user_blueprint.route("/dashboard")
 def user_dashboard():
     library = current_app.config["LIBRARY"]
     user_manager = current_app.config["USER_MANAGER"]
 
-    # For now, assume user with ID=1 is logged in
+    # For now, user with ID=1 is logged in
     user = user_manager.find_user(1)
 
     borrowed_books = []
@@ -17,13 +17,13 @@ def user_dashboard():
 
     return render_template("user/dashboard.html",user=user,borrowed_books=borrowed_books)
 
-# 📖 View Books
+
 @user_blueprint.route("/books", methods=["GET"])
 def view_books():
     library = current_app.config["LIBRARY"]
     return render_template("user/books_user.html", books=library.books)
 
-# ✅ Borrow Book
+
 @user_blueprint.route("/books/borrow/<isbn>", methods=["POST"])
 def borrow_book(isbn):
     library = current_app.config["LIBRARY"]
@@ -35,7 +35,7 @@ def borrow_book(isbn):
 
     return redirect(url_for("user.view_books"))
 
-# 🔄 Return Book
+
 @user_blueprint.route("/books/return/<isbn>", methods=["POST"])
 def return_book(isbn):
     library = current_app.config["LIBRARY"]
